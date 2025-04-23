a7db73d99a7c51e769f79b95b5fc2ce3f824d140
AKASH RAI <akashrai4546@gmail.com>
2025-04-24 00:09:06 +0530

    feat: initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+import type { Metadata } from "next";
+
+const satoshi = localFont({
+  src: "./fonts/Satoshi-Variable.ttf",
+  variable: "--font-satoshi",
+  weight: "100 900 500 300 400 200 600 700 800",
+});
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Cognify",
+  description: "An AI-powered notes application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${satoshi.variable} ${poppins.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
